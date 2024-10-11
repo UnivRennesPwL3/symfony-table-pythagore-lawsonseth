d45270097a0242881458002051fffec712f02f15
Seth Lawson <latesethlawsonhetchely@gmail.com>
2024-10-11 18:52:34 +0200

    ..

--- src/Utils/PythagoreUtility.php
+++ src/Utils/PythagoreUtility.php
@@ -4,48 +4,53 @@ namespace App\Utils;
 
 class PythagoreUtility
 {
+    public function display(): string
+    {
+      $length = 10;
+      
+      $html = "<style>
+                    table.pythagore-table {
+                        border: 1px solid black;
+                        border-collapse: collapse;
+                    }
+                    table.pythagore-table td {
+                        border: 1px solid black;
+                        padding: 5px;
+                        text-align: center;
+                    }
+                </style>";
 
-    public function display():string {
-        $length = 10;
+        $html .= "<table class='pythagore-table'>";
 
-        $html = "<table>";
-        for ($i=0; $i <= $length; $i++) { 
+        for ($i = 0; $i <= $length; $i++) {
+            $html .= "<tr>";
 
-          $html .=  "<tr>";
+            for ($j = 0; $j <= $length; $j++) {
 
+                if ($i == 0 && $j == 0) {
+                    $html .= "<td>0</td>";
+                }
+                elseif ($i == $j) {
+                    $html .= "<td>X</td>";
+                }
+                elseif ($i == 0 && $j != 0) {
+                    $html .= "<td>$j</td>";
+                }
+                elseif ($i != 0 && $j == 0) {
+                    $html .= "<td>$i</td>";
+                }
+                else {
+                    $html .= "<td>" . $i * $j . "</td>";
+                }
+            }
 
-            for ($j=0; $j <= $length; $j++) { 
-              if($i==0 && $j==0){
-                $html .= "<td>0</td>";
-              }
-              elseif($i==$j){
-                $html .= "<td>X</td>";
-              }
-              elseif($i==0 && $j!=0){
-                $html .= "<td>$j</td>";
-              }
-              elseif($i!=0 && $j==0){
-                $html .= "<td>$i</td>";
-              }
-              else {
-                $html .= "<td>".$i*$j."</td>";
-              }
-            } 
-              
-            $html .= "</tr>";
-            $html .= "</table>";
+            $html .= "</tr>"; 
+        }
 
-        
-     }
-
-
+        $html .= "</table>";
 
         return $html;
- 
     }
-   
-
-
 }
 
 ?>
